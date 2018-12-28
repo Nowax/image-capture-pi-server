6c1041ad7e8484dd16cda6630b7b72f401ffa7ed
Wojciech Nowak <wojciech.nowak@siili.com>
2018-12-29 00:58:41 +0100

    prettier only

--- src/ImagesRecorder.js
+++ src/ImagesRecorder.js
@@ -1,79 +1,81 @@
 // @flow
-const NodeWebcam = require( "node-webcam" );
-const dayjs = require("dayjs")
-const advancedFormat = require ("dayjs/plugin/advancedFormat")
+const NodeWebcam = require('node-webcam');
+const dayjs = require('dayjs');
+const advancedFormat = require('dayjs/plugin/advancedFormat');
 const jimp = require('jimp');
 
 let defaultOptions = {
- 
     //Picture related
     width: 320,
     height: 240,
     quality: 100,
- 
+
     //Delay to take shot
     delay: 0,
- 
+
     //Save shots in memory
     saveShots: true,
- 
+
     // [jpeg, png] support varies
     // Webcam.OutputTypes
-    output: "png",
- 
+    output: 'png',
+
     //Which camera to use
     //Use Webcam.list() for results
     //false for default device
     device: false,
- 
+
     // [location, buffer, base64]
     // Webcam.CallbackReturnTypes
-    callbackReturn: "buffer",
- 
+    callbackReturn: 'buffer',
+
     //Logging
     verbose: false
 };
 
 const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
-const todayUTC = () => dayjs().format("YYYY-MM-DD")
+const todayUTC = () => dayjs().format('YYYY-MM-DD');
 class ImageRecorder {
     constructor(service, owner) {
-        this.webcam = NodeWebcam.create( defaultOptions );
-        this.firebase = service
-        this.owner = owner
-        dayjs.extend(advancedFormat)
+        this.webcam = NodeWebcam.create(defaultOptions);
+        this.firebase = service;
+        this.owner = owner;
+        dayjs.extend(advancedFormat);
     }
 
     async start() {
-        this.captureImage()
-
         while (true) {
-            console.log('Reloop')
-            this.captureImage()
-            await snooze(15 * 1000)
+            console.log('Reloop');
+            this.captureImage();
+            await snooze(15 * 1000);
         }
-    };
+    }
 
     captureImage() {
-        this.webcam.capture("temporary_image", (err, data) => {
+        this.webcam.capture('temporary_image', (err, data) => {
             jimp.read(data)
-                .then( image => {
+                .then(image => {
                     return image
                         .grayscale()
                         .resize(640, 480)
                         .quality(60)
-                        .write("temporary_image.jpg")
-                    })
-                .then( () => {
-                    this.firebase.upload("temporary_image.jpg", `${this.owner}-${todayUTC()}`,
-                        this.generateRandomFileName("image"), this.owner, todayUTC())
+                        .write('temporary_image.jpg');
                 })
-                .catch( err => console.log(err))
-        })
+                .then(() => {
+                    this.firebase.upload(
+                        'temporary_image.jpg',
+                        `${this.owner}-${todayUTC()}`,
+                        this.generateRandomFileName('image'),
+                        this.owner,
+                        todayUTC()
+                    );
+                })
+                .catch(err => console.log(err));
+        });
     }
 
     generateRandomFileName(name) {
-        const todayUTC = dayjs().format("-YYYY-MM-DD--kk-mm-ss")
+        const todayUTC = dayjs().format('-YYYY-MM-DD--kk-mm-ss');
         return `${name}${todayUTC}.jpg`;
     }
 }
